22de9f46113420fe8f32dc1bb6e9dc641694205c
TENSHON <mckenzieraniel@gmail.com>
2026-06-22 14:39:28 +0000

    Build fix: Include generated Convex types in repository

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,65 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as ResendOTP from "../ResendOTP.js";
+import type * as auth from "../auth.js";
+import type * as files from "../files.js";
+import type * as fix from "../fix.js";
+import type * as http from "../http.js";
+import type * as purchases from "../purchases.js";
+import type * as seeder from "../seeder.js";
+import type * as songs from "../songs.js";
+import type * as users from "../users.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  ResendOTP: typeof ResendOTP;
+  auth: typeof auth;
+  files: typeof files;
+  fix: typeof fix;
+  http: typeof http;
+  purchases: typeof purchases;
+  seeder: typeof seeder;
+  songs: typeof songs;
+  users: typeof users;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
